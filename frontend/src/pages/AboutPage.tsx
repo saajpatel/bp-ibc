8700462f9628c4f16406fd2dfa3f882963cef154
Qingyao Liu <qingyao.liu.c@gmail.com>
2026-01-16 22:45:07 -0500

    AboutIBCSection added with the css and tsx.

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,10 +1,11 @@
 import Footer from '../components/site/Footer'
+import AboutIBCSection from './about_page_sections/AboutIBCSection'
 function AboutPage() {
     return (
         <>
             <h1>Welcome to the About Page</h1>
             <p>This is the main content area.</p>
-            <Footer/> 
+            <Footer/>
         </>
     )
 }
