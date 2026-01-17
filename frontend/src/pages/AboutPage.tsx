c930b27251893ca11d3962643be27581092f812b
Qingyao Liu <qingyao.liu.c@gmail.com>
2026-01-17 18:11:35 -0500

    Main changes: - Education section component added. - Education section added to About Page. - Placeholder text in About Page removed.
    Misc changes:
    - CSS files for the other sections of the About Page refactored for consistency and adaptibility.

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,14 +1,14 @@
 import Footer from '../components/site/Footer'
 import AboutIBCSection from '../components/site/AboutIBCSection'
 import MissionSection from '../components/site/MissionSection'
+import EducationSection from '../components/site/EducationSection'
 
 function AboutPage() {
     return (
         <>
-            <h1>Welcome to the About Page</h1>
-            <p>This is the main content area.</p>
             <AboutIBCSection/>
             <MissionSection/>
+            <EducationSection/>
             <Footer/>
         </>
     )
