60562c524b06168ae520477580b3148e7fe50869
Tiffany Li <tiffanylii019@gmail.com>
2026-01-22 22:08:20 -0500

    Added top section of education page. (forgot to commit earlier lol)

---
+++ frontend/src/components/pagesections/WildlifeGuide.tsx
@@ -0,0 +1,20 @@
+import './WildlifeGuide.css' 
+
+function WildlifeGuide () {
+    console.log("wildlife section");
+    return (
+        <section id="WildlifeGuidance"
+            style={{
+                padding:'2em 4em',
+                fontFamily: 'DM Sans, sans-serif'}}>
+            <h1 id="WildlifeGuidanceTitle">Wildlife Guidance</h1>
+            <p className="subtext">If you've found a wild animal, it's important to pause before taking action. 
+                                    Many animals that appear abandoned or injured are behaving normally. 
+                                    This page will help you decide what to do - safely and responsibly.</p>
+            {/* <p className="subtext">that appear abandoned or injured are behaving normally. This page will help you decide</p>
+            <p className="subtext">what to do - safely and responsibly.</p> */}
+        </section>
+    );
+}
+
+export default WildlifeGuide;
--- frontend/src/pages/EducationPage.tsx
+++ frontend/src/pages/EducationPage.tsx
@@ -1,10 +1,12 @@
 import Footer from '../components/site/Footer'
+import WildlifeGuide from '../components/pagesections/WildlifeGuide'
 
 function EducationPage() {
     return (
         <>
             <h1>Welcome to the Education Page</h1>
             <p>This is the main content area.</p>
+            <WildlifeGuide/>
             <Footer/> 
         </>
     )
