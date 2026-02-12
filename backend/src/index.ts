3980d6cda61fb8cb2f32a815a3f1da9eec5f5452
Vicheka Oeun <oeunvicheka95@gmail.com>
2026-02-12 00:41:12 -0500

    update db and backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,10 @@ import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 import sectionRoute from './routes/sectionRoute';
-import pageRoute from './routes/pageRoute'
-import draftRoute from './routes/draftRoute'
+import pageRoute from './routes/pageRoute';
+import draftRoute from './routes/draftRoute';
+import siteRoute from './routes/siteRoute';
+import publishedRoute from './routes/publishedRoute';
 
 dotenv.config();
 
@@ -19,10 +21,12 @@ app.get('/api/health', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
+app.use('/api', siteRoute);
+app.use('/api', draftRoute);
+app.use('/api', pageRoute);
+app.use('/api', sectionRoute);
+app.use('/api', publishedRoute);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use('/api', sectionRoute);
-app.use('/api', pageRoute);
-app.use('/api', draftRoute);
